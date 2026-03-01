b8d120781b069e3c7405870a9b087c3e3e777904
bekohaty <bekohaty@gmail.com>
2026-03-01 07:19:50 +0200

    fix

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,4 +3,6 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "ar"],
   defaultLocale: "en",
+  // Check NEXT_LOCALE cookie first, then Accept-Language header
+  localeDetection: true,
 });
